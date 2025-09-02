2f99b8d270dffcf792d2dbda17b728747b80af19
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-09-02 15:03:13 +0200

    feat: create ActorCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import ActorCard from '../components/ActorCard';
 
 
 function App() {
+  const [actors, setActors] = useState([])
   useEffect(() => {
     axios.get("https://lanciweb.github.io/demo/api/actors/")
-      .then((res) => console.log("Actors:", res.data));
+      .then((res) => setActors(res.data))
   }, []);
 
   return (
+    <>
+      <div className="container py-4">
+        <h1>Actors & Actresses</h1>
+        <div className="row">
+          {actors.map((actor) => {
+            return (
+              < ActorCard key={actor.id} actor={actor} />)
 
-    <div className="container py-4">
-      <h1>Actors & Actresses</h1>
-    </div>
+          })}
 
+        </div>
+      </div>
+
+    </>
   )
 }
 
