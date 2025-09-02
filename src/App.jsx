513a12d9797d7b99a358a20e06a99e148dc28867
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-09-02 13:58:33 +0200

    feat: add API fetching for actors with axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import axios from 'axios'
 
 
 function App() {
-
+  useEffect(() => {
+    axios.get("https://lanciweb.github.io/demo/api/actors/")
+      .then((res) => console.log("Actors:", res.data));
+  }, []);
 
   return (
-    <>
-      
-    </>
+
+    <div className="container py-4">
+      <h1>Actors & Actresses</h1>
+    </div>
+
   )
 }
 
